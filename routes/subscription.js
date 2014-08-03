f0df02a1b9599fbd28281d1434d843c94f7a4b91
Ed Chen <echen@paysimple.com>
2014-08-03 15:27:31 -0700

    multiple emails

--- routes/subscription.js
+++ routes/subscription.js
@@ -8,7 +8,7 @@ function subscription (req, res, config) {
 
     var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
     sendgrid.send({
-        to:       'feisajan@yahoo.com; mredwardchen@hotmail.com',
+        to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
         from:     'csr2014@xinnamonj.com',
         subject:  'Mail test',
         text:     'My first email through SendGrid.'
