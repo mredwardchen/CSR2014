968329458671ae5e0226d1e5d9f57ccf56e84c87
Ed Chen <echen@bluekai.com>
2014-08-05 12:25:49 -0700

    querystring parsing

--- routes/subscription.js
+++ routes/subscription.js
@@ -2,9 +2,22 @@
 
 module.exports = subscription;
 
-// getting our contacts from the 'database' - contacts.json
+var queryString = require('querystring')
 
 function subscription (req, res, config) {
+
+    function handleParams(params) {
+        var result = '';
+        console.log('before:'+params);
+        for (var key in params) {
+            console.log('key:'+key);
+            if (params.hasOwnProperty(key)) {
+                result += key + ' = ' + params[key] ;
+            }
+        }
+        console.log('after:'+result);
+        return result;
+    }
 
     function handleForm (req, res) {
         var chunk='';
@@ -17,12 +30,14 @@ function subscription (req, res, config) {
         req.on('readable', function() {
             while (null !== (chunk = req.read())) {
                 console.log('got %d bytes of data. data==> %s', chunk.length, chunk);
+                var params = queryString.parse(chunk.toString());
+                console.log('form params: %s', handleParams(params));
                 var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
                 sendgrid.send({
                     to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
                     from:     'csr2014@xinnamonj.com',
                     subject:  'CSR new subscriber notice',
-                    text:     'New subscriber data:'+chunk
+                    text:     'New subscriber data:'+params.toString()
                 }, function(err, json) {
                     if (err) { console.error(err); }
                     console.log(json);
