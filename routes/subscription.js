7d90b7798ea23ec8aa7c87e28480ac992fc5a8d3
Ed Chen <echen@paysimple.com>
2014-08-05 08:23:09 -0700

    test chunk

--- routes/subscription.js
+++ routes/subscription.js
@@ -17,17 +17,17 @@ function subscription (req, res, config) {
         req.on('readable', function() {
             while (null !== (chunk = req.read())) {
                 console.log('got %d bytes of data. data==> %s', chunk.length, chunk);
+                var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
+                sendgrid.send({
+                    to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
+                    from:     'csr2014@xinnamonj.com',
+                    subject:  'CSR new subscriber notice',
+                    text:     'New subscriber data:'+chunk
+                }, function(err, json) {
+                    if (err) { console.error(err); }
+                    console.log(json);
+                });
             }
-        });
-        var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
-        sendgrid.send({
-            to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
-            from:     'csr2014@xinnamonj.com',
-            subject:  'CSR new subscriber notice',
-            text:     'New subscriber data:'+chunk
-        }, function(err, json) {
-            if (err) { console.error(err); }
-            console.log(json);
         });
 
         res.template('subscriptionSent.ejs', {});
