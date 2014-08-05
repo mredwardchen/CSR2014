87ced3cba9787fecce03682dea1afd72e8daa1a1
Ed Chen <echen@paysimple.com>
2014-08-05 08:33:16 -0700

    chunk

--- routes/subscription.js
+++ routes/subscription.js
@@ -12,22 +12,22 @@ function subscription (req, res, config) {
            console.log('error');
         });
         req.on('end', function() {
-            console.log('there will be no more data. Sending email.');
-            var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
-            sendgrid.send({
-                to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
-                from:     'csr2014@xinnamonj.com',
-                subject:  'CSR new subscriber notice',
-                text:     'New subscriber data:'+chunk
-            }, function(err, json) {
-                if (err) { console.error(err); }
-                console.log(json);
-                res.template('subscriptionSent.ejs', {});
-            });
+            console.log('there will be no more data.');
         });
         req.on('readable', function() {
             while (null !== (chunk = req.read())) {
                 console.log('got %d bytes of data. data==> %s', chunk.length, chunk);
+                var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
+                sendgrid.send({
+                    to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
+                    from:     'csr2014@xinnamonj.com',
+                    subject:  'CSR new subscriber notice',
+                    text:     'New subscriber data:'+chunk
+                }, function(err, json) {
+                    if (err) { console.error(err); }
+                    console.log(json);
+                    res.template('subscriptionSent.ejs', {});
+                });
             }
         });
 
