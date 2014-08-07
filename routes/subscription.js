8d621013ec14f9d070f1c5daca727651aa5306ae
Ed Chen <echen@paysimple.com>
2014-08-07 08:26:11 -0700

    process email body with ejs

--- routes/subscription.js
+++ routes/subscription.js
@@ -5,39 +5,71 @@ module.exports = subscription;
 var queryString = require('querystring')
 
 function subscription (req, res, config) {
+    var params = [
+        {'title': 'Edition', 'key': 'F_04'},
+        {'title': 'First Name', 'key': 'a_first_name'},
+        {'title': 'Last Name', 'key': 'a_last_name'},
+        {'title': 'Title', 'key': 'a_title'},
+        {'title': 'Company', 'key': 'a_company'},
+        {'title': 'Address', 'key': 'a_address_1'},
+        {'title': '', 'key': 'a_address_2'},
+        {'title': '', 'key': 'a_address_2'},
+        {'title': 'City', 'key': 'a_city'},
+        {'title': 'State', 'key': 'a_state'},
+        {'title': 'Zip/Postal Code', 'key': 'a_zip'},
+        {'title': 'Country', 'key': 'a_province'},
+        {'title': 'Email', 'key': 'a_email'},
+        {'title': 'Phone', 'key': 'a_phone'},
+        {'title': 'Fax', 'key': 'a_fax'},
+        {'title': '01. Assembly of IC Packages', 'key': 'F_03'},
+        {'title': '02. Primary Industry:', 'key': 'F_14'},
+        {'title': '02. Primary Industry(other)', 'key': 'F_14_OTHER'},
+        {'title': '03. Primary Job Function', 'key': 'F_15'},
+        {'title': '03. Primary Job Function(other)', 'key': 'F_15_OTHER'},
+        {'title': '04. What products do you buy/sell?', 'key': 'F_17[]'},
+        {'title': '05. Number of Employees', 'key': 'F_06'},
+        {'title': '06. Please indicate Month of Birth', 'key': 'F_07'},
+        {'title': '07. May we contact you by email about your subscription?', 'key': 'F_08'}
+    ];
 
-    function handleParams(params) {
-        var result = '';
-        console.log('before:'+params);
-        for (var key in params) {
-            console.log('key:'+key);
-            if (params.hasOwnProperty(key)) {
-                result += key + ' = ' + params[key] ;
-            }
+    function processParams(p) {
+        // update values from form to key array
+        for (var i=0; i<params.length; i++) {
+            params[i].value = p[params[i].key];
+            console.log('%s set to %s',params[i].key, params[i].value);
         }
-        console.log('after:'+result);
-        return result;
+    }
+
+    function toEmailBody(p) {
+        var data = params || p;
+
+        var text = new EJS({url: '../templates/subscriptionEmailBody.ejs'}).render({'data': data});
+
+        console.log('Email body: %s', text);
+
+        return text;
     }
 
     function handleForm (req, res) {
         var chunk='';
         req.on('error', function() {
-           console.log('error');
+           console.log('error receiving form data');
         });
         req.on('end', function() {
-            console.log('there will be no more data.');
+            //console.log('there will be no more data.');
         });
         req.on('readable', function() {
             while (null !== (chunk = req.read())) {
                 console.log('got %d bytes of data. data==> %s', chunk.length, chunk);
-                var params = queryString.parse(chunk.toString());
-                console.log('subscriber params: %s', handleParams(params));
+                var p = queryString.parse(chunk.toString());
+                processParams(p);
+
                 var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME || 'w7y2ublh', process.env.SENDGRID_PASSWORD || 'app28001807@heroku.com');
                 sendgrid.send({
                     to:       ['feisajan@yahoo.com','mredwardchen@hotmail.com'],
                     from:     'csr2014@xinnamonj.com',
                     subject:  'CSR new subscriber notice',
-                    text:     'New subscriber data:'+params.toString()
+                    text:     'New subscriber data:' + toEmailBody(params)
                 }, function(err, json) {
                     if (err) { console.error(err); }
                     res.template('subscriptionSent.ejs', {'params': params});
