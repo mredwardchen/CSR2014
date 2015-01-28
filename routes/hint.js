a8b5faa18617dca4cb537c544e8f93420fdec5c6
Ed Chen <echen@bluekai.com>
2015-01-28 15:28:39 -0800

    breakup geo into country region and city

--- routes/hint.js
+++ routes/hint.js
@@ -21,6 +21,7 @@ function hint (req, res, config) {
     var host = req.headers['host'] ? req.headers['host'] : 'NA';
     var referer = req.headers['referer'] ? req.headers['referer'] : 'NA';
     var ua = req.headers['user-agent'] ? req.headers['user-agent'] : 'NA';
+    var remoteAddr = req.headers['REMOTE_ADDR'] ? req.headers['REMOTE_ADDR'] : 'NA';
     var url = req.url; // '/csrhint/site/2?ret=html&like=foo%3Dbar&like=__xj_t%3DChip%20Scale%20Review%20Magazine%20-%20The%20International%20Magazine%20for%20Device%20and%20Wafer-Level%20Test%2C%20Assembly%2C%20and%20Packaging%20Addressing%20High-density%20Interconnection%20of%20Microelectronic%20IC%27s%20including%203D%20packages%2C%20MEMS%2C%20MOEMS%2C%20RF%2FWireless%2C%20Optoelectronic%20and%20Other%20Wafer-fabricated%20Devices%20for%20the%2021st%20Century&like=__xj_k%3DChip%20Scale%20Review%20Magazine%2C%20Device%20test%2C%20Wafer-Level%20Test%2C%20wafer%20Assembly%2C%20wafer%20Packaging%2C%20High-density%20Interconnection%2C%20Microelectronic%2C%20IC%2C%203D%20packages%2C%20MEMS%2C%20MOEMS%2C%20RF%2FWireless%2C%20Optoelectronic%2C%20semiconductor%2C%20wafer%20manufacturing%2C%20wafer%20fabrication&like=__xj_l%3Dhttp%3A%2F%2Flocalhost%3A5000%2Fabout.html&xjuid=1451044903&r=18338017';
 
     // parse user ID
@@ -36,7 +37,11 @@ function hint (req, res, config) {
     var clientIp = requestIp.getClientIp(req); // on loaclhost > 127.0.0.1
     var geo = geoip.lookup(clientIp);
     if (!geo) geo = 'NA';
-    data['geo'] = geo;
+    //data['geo'] = geo;
+    data['geo'] = remoteAddr;
+    data['country'] = geo['country'] ? geo['country'] : 'NA';
+    data['region'] = geo['region'] ? geo['region'] : 'NA';
+    data['city'] = geo['city'] ? geo['city'] : 'NA';
     //console.log('Geo==>'+geo);
 
     // parse user agent
